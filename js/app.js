7ae9969a096144e3d1c8ac6236d86f7646813003
Antonio Valle <juan.valle.d1@gmail.com>
2024-01-29 16:33:04 -0600

    Job elapsed time fixed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-const form = document.querySelector('.contact-form');
-const submitBtn = document.getElementById('submit-btn');
+const form = document.querySelector('.contact-form')
+const submitBtn = document.getElementById('submit-btn')
+const currentJobTimmer = document.getElementById('current-job-timmer')
 
-var name2 = document.getElementById('name2');
-var company2 = document.getElementById('company2');
-var email2 = document.getElementById('email2');
-var message2 = document.getElementById('message2');
+var name2 = document.getElementById('name2')
+var company2 = document.getElementById('company2')
+var email2 = document.getElementById('email2')
+var message2 = document.getElementById('message2')
+
+
+window.addEventListener('load', () => {
+    // Set current job elapsed time
+    currentJobTimmer.innerText = getElapsedTime()
+})
 
 
 submitBtn.addEventListener('click', (event) => {
@@ -24,4 +31,39 @@ submitBtn.addEventListener('click', (event) => {
     message2.value = message;
 
     form.submit();
-});
+})
+
+
+
+// Get current job elapsed time
+const getElapsedTime = () => {
+    const startDate = new Date("2022-12-14")
+
+    const diff = parseInt((new Date - startDate) / (1000 * 60 * 60 * 24))
+
+    const years = parseInt(diff / 365)
+    const months = parseInt((diff % 365) / 30)
+
+    let elapsedYears 
+    let elapsedMonths
+
+    if (years === 0) {
+        elapsedYears = ''
+    } else if (years === 1) {
+        elapsedYears = `${years} year`
+    } else {
+        elapsedYears = `${years} years`
+    }
+
+    if (months === 0) {
+        elapsedMonths = ''
+    } else if (months === 1) {
+        elapsedMonths = `${months} month`
+    } else {
+        elapsedMonths = `${months} months`
+    }
+
+    const elapsedTime = `${elapsedYears} ${elapsedMonths}`
+
+    return elapsedTime
+}
